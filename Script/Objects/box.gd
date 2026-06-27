097e10dc33355ef7e48a2c36307d2e494374c3c1
イアン <janivantot@gmail.com>
2026-06-27 22:15:19 +0800

    Object Template
    Created Signal Object Template

---
+++ Script/Objects/box.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	print("enter")
+	pass # Replace with function body.
+
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	print("exit")
+	pass # Replace with function body.
